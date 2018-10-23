b4acc090cb39640124afb6beabbf3b37d59d2801
Stevman17 <43613675+Stevman17@users.noreply.github.com>
2018-10-23 15:55:26 -0500

    Create Iris_Flowers_Machine_Learning.py

---
+++ Iris_Flowers_Machine_Learning.py
@@ -0,0 +1,126 @@
+ Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy as np
+print('numpy: {}'.format(np.__version__))
+# matplotlib
+import matplotlib as plt
+print('matplotlib: {}'.format(plt.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+from pandas.plotting import scatter_matrix
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+# shape
+print(dataset.shape)
+# head
+print(dataset.head(20))
+# descriptions
+print(dataset.describe())
+# descriptions
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+
+# histograms
+dataset.hist()
+
+#scatter plot matrix
+scatter_matrix(dataset)
+
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+#We will use 10-fold cross validation to estimate accuracy.
+
+#This will split our dataset into 10 parts, train on 9 and test on 1 and repeat for all combinations of train-test splits.
+
+
+# Test options and evaluation metric
+#The specific random seed does not matter
+seed = 7
+scoring = 'accuracy'
+
+#Let’s evaluate 6 different algorithms:
+
+#Logistic Regression (LR)
+#Linear Discriminant Analysis (LDA)
+#K-Nearest Neighbors (KNN).
+#Classification and Regression Trees (CART).
+#Gaussian Naive Bayes (NB).
+#Support Vector Machines (SVM).
+#This is a good mixture of simple linear (LR and LDA), nonlinear (KNN, CART, NB and SVM) algorithms.
+# We reset the random number seed before each run to ensure that the evaluation of each algorithm is performed using exactly the same data splits.
+# It ensures the results are directly comparable.
+
+#Let’s build and evaluate our five models:
+# Spot Check Algorithms
+gamma = 'Auto'
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+plt.close()
+# Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+
+#The KNN algorithm was the most accurate model that we tested. Now we want to get an idea of the accuracy of the
+#  model on our validation set.
+#We can run the KNN model directly on the validation set and summarize the results as a final accuracy score, a
+# confusion matrix and a classification report.
+
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+
